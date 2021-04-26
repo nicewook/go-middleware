7fa59be9390f46066fef1567d6240dabe9b7b070
hsjeong <nicewook@hotmail.com>
2021-04-26 15:08:22 +0900

    init echo framework

--- middle.go
+++ middle.go
@@ -1,46 +1,22 @@
 package main
 
 import (
-	"log"
 	"net/http"
+
+	"github.com/labstack/echo"
 )
 
-func CheckAuthentication(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("start CheckAuthentication")
-		if r.Header.Get("Client-ID") != "hsjeong" {
-			http.Error(w, "Client not found", http.StatusForbidden)
-			return
-		}
-		if r.Header.Get("Client-Access-ID") != "hsjeong-access" {
-			http.Error(w, "Invalid secret key", http.StatusForbidden)
-			return
-		}
-		if r.Header.Get("Content-Type") != "application/json" {
-			http.Error(w, "Not proper content type", http.StatusForbidden)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-		log.Println("--end CheckAuthentication")
-	})
+type Response struct {
+	Code    int    `json:"code`
+	Message string `json:"message"`
 }
 
-func final(w http.ResponseWriter, r *http.Request) {
-	log.Println("finalHandler")
-	w.Write([]byte("finalHandler"))
+func Ping(c echo.Context) error {
+	return c.JSON(http.StatusOK, Response{101, "pong"})
 }
 
 func main() {
-	mux := http.NewServeMux()
-
-	finalHandler := http.HandlerFunc(final)
-
-	mux.Handle("/", CheckAuthentication(finalHandler))
-
-	port := "3000"
-	log.Println("listening on: ", port)
-	if err := http.ListenAndServe(":"+port, mux); err != nil {
-		log.Fatal(err)
-	}
+	e := echo.New()
+	e.GET("/ping", Ping)
+	e.Logger.Fatal(e.Start(":1234"))
 }
