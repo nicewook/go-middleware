1e7a8a5f19fddf73283e37789333b62072982616
hsjeong <nicewook@hotmail.com>
2021-04-26 15:50:57 +0900

    echo log middleware

--- middle.go
+++ middle.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
 )
@@ -11,12 +14,47 @@ type Response struct {
 	Message string `json:"message"`
 }
 
+type LogFilter struct {
+	Level   string `json:"level"`
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	UserID  string `json:"UserID"`
+}
+
+func logMiddleWare(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var err error
+		start := time.Now()
+		if err = next(c); err != nil {
+			return err
+		}
+		diff := time.Since(start)
+		req := c.Request()
+		resp := c.Response()
+		clog, _ := json.Marshal(c.Get("clog"))
+		fmt.Println(
+			start.Format(time.RFC3339),
+			diff,
+			c.RealIP(),
+			req.Method,
+			req.Host,
+			req.Header.Get(echo.HeaderContentLength),
+			resp.Size,
+			string(clog),
+		)
+		return err
+	}
+}
+
 func Ping(c echo.Context) error {
+	time.Sleep(20 * time.Millisecond)
+	c.Set("clog", LogFilter{"INFO", 101, "pong", "hsjeong@gmail.com"})
 	return c.JSON(http.StatusOK, Response{101, "pong"})
 }
 
 func main() {
 	e := echo.New()
+	e.Use(logMiddleWare)
 	e.GET("/ping", Ping)
 	e.Logger.Fatal(e.Start(":1234"))
 }
